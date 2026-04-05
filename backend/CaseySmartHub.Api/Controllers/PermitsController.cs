b67c9e11bd9e4913b015e45d8efa003063555abb
prod-chirag <chiragluitel32@gmail.com>
2026-04-05 13:54:46 +1000

    backend setup complete v1.0.0

---
+++ backend/CaseySmartHub.Api/Controllers/PermitsController.cs
@@ -0,0 +1,40 @@
+using CaseySmartHub.Application.Features.Permits.Commands;
+using CaseySmartHub.Domain.Exceptions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CaseySmartHub.Api.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class PermitsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public PermitsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost("save-to-user")]
+    public async Task<IActionResult> SaveUserPermit([FromBody] SaveUserPermitCommand command)
+    {
+        try
+        {
+            var success = await _mediator.Send(command);
+            if (success)
+            {
+                return Ok(new { message = "Permit successfully saved to user profile." });
+            }
+            return BadRequest(new { error = "Failed to save the permit." });
+        }
+        catch (PermitNotFoundException ex)
+        {
+            return NotFound(new { error = ex.Message });
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new { error = "An unexpected error occurred." + ex.Message });
+        }
+    }
+}
--- backend/CaseySmartHub.Api/Program.cs
+++ backend/CaseySmartHub.Api/Program.cs
@@ -1,23 +1,56 @@
+using CaseySmartHub.Application.Features.Permits.Commands;
+using CaseySmartHub.Application.Interfaces.ExternalServices;
+using CaseySmartHub.Application.Interfaces.Repositories;
+using CaseySmartHub.Infrastructure.ExternalServices;
+using CaseySmartHub.Infrastructure.ExternalServices.Base;
+using CaseySmartHub.Infrastructure.Persistence;
+using CaseySmartHub.Infrastructure.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// ========
+// 1. Database Services - One time setup.  
+// ========
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("PsqlConnectionString")));
 
+// ========
+// 2. Internal Database Repositories
+// ========
+builder.Services.AddScoped<IPermitRepository, PermitRepository>();
+
+// ========
+// 3. External API Services
+// ========
+//Base HTTP Client Setup - One time setup.
+builder.Services.AddHttpClient<CaseyOpenDataClient>();
+
+builder.Services.AddScoped<ICaseyPermitAPIService, CaseyPermitAPIService>();
+
+// ========
+// 4. Application Layer Services for MediatR
+// ========
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(SaveUserPermitCommand).Assembly));
+
+// ========
+// 5. Constrollers (our APIs)
+// ========
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi(); //For Swagger
 
+// ========
+// 6. Standard - Don't Change
+// ========
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(options => {options.SwaggerEndpoint("/openapi/v1.json", "Casey Smart Hub API");});
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
