2fb6bd2c7e2d6717de6cdad84a8761768e385a07
Ozboy32 <dylanmusgrave32@gmail.com>
2026-04-08 19:07:14 +1000

    Setup project for settings page

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,4 +1,6 @@
-import { Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { View } from "react-native";
+import SettingsIndex from "./(settings)";
 
 export default function Index() {
   return (
@@ -9,7 +11,8 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <SettingsIndex />
+      <StatusBar style="auto" />
     </View>
   );
 }
