84350251732b834be6a764b186a851fbb5d2ba29
Rhyslan <79964831+Rhyslan@users.noreply.github.com>
2026-08-27 14:59:25 +1000

    Adjust contrast

--- frontend/src/components/Map/components/ConnectionBanner.tsx
+++ frontend/src/components/Map/components/ConnectionBanner.tsx
@@ -4,9 +4,11 @@ import { Text, View } from 'react-native';
 export default function ConnectionBanner({ ok }: { ok: boolean }) {
   if (ok) return null;
   return (
-    <View className="absolute self-center top-14 bg-warning-100 dark:bg-dark-warning-200 rounded-full px-4 py-2 shadow-md z-50">
-      <Text className="text-xs font-semibold text-warning-700 dark:text-dark-warning-700">
-        Backend not reached. Please restart app.
+    <View className="absolute self-center top-14 bg-warning-200 dark:bg-dark-warning-200 rounded-full px-4 py-2 shadow-md z-50">
+      <Text className="text-sm font-semibold text-warning-700 dark:text-dark-warning-800 text-center">
+        Backend not reached. 
+        {"\n"}
+        Please restart app or try again later.
       </Text>
     </View>
   );
